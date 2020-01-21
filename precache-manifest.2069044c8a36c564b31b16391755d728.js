5bcbd105395c43daf4f0d91fb80f513ff97cf621
unknown <you@example.com>
2020-01-21 17:56:01 +0600

    Updates

---
+++ precache-manifest.2069044c8a36c564b31b16391755d728.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef4714fc6a4c29f6f571edb4aec90fd",
+    "url": "/Monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e74df0def81f70953803",
+    "url": "/Monsters-rolodex/static/css/main.ccf31ba0.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/Monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e74df0def81f70953803",
+    "url": "/Monsters-rolodex/static/js/main.dc03e8d7.chunk.js"
+  },
+  {
+    "revision": "42946c634c7e2f4eebad",
+    "url": "/Monsters-rolodex/static/js/runtime-main.f0776dd1.js"
+  }
+]);
